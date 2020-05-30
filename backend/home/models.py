732a8587de6a0051b0fcebd83c7ba539970db171
Crowdbotics <team@crowdbotics.com>
2020-05-30 07:16:15 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,24 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
+    gfgfd = models.ManyToManyField(
+        "home.HomePage", blank=True, related_name="customtext_gfgfd",
+    )
+    tfryrhgf = models.ManyToManyField(
+        "users.User", blank=True, related_name="customtext_tfryrhgf",
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +32,8 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
--- backend/mobile_30_dev_5255/wsgi.py
+++ backend/mobile_30_dev_5255/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobile_30_dev_5255.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobile_30_dev_5255.settings")
 
 application = get_wsgi_application()
